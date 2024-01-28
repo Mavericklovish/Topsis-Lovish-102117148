c8ecfbb984d3ed30c83e9dd7d27bc8e5a9330ae8
Mavericklovish <100517147+Mavericklovish@users.noreply.github.com>
2024-01-29 02:48:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Lovish Bansal',                   # Type in your name
   author_email = 'bansallovish612@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Mavericklovish/Topsis-Lovish-102117148',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Mavericklovish/Topsis-Lovish-102117148.git',    # I explain this later on
+  download_url = 'https://github.com/Dhruv-os-23/Topsis-Dhruv-102117061/archive/refs/tags/1.0.4.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
